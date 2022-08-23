30804f577dd7ed865cfa88c15b45ca50beef6af4
unknown <luis.daniel.tang@gmail.com>
2022-08-23 12:37:05 -0300

    casi

--- components/addTask.js
+++ components/addTask.js
@@ -2,35 +2,36 @@ import checkComplete from "./checkcomplete.js";
 import deleteIcon from "./deleteIcon.js";
 
 export const addTask = (evento) => {
-    const list = document.querySelector('[data-list]');
-    const task = createTask(evento);
-    list.appendChild(task);
-  };
-  
-  const createTask = (evento) => {
     evento.preventDefault();
-    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-    console.log(taskList);
+    const list = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const calendar = document.querySelector('[data-form-date]');
+
     const value = input.value;
     const date = calendar.value;
     const dateFormat = moment(date).format('DD/MM/YYYY');
-    console.log(dateFormat);
+
+    input.value = '';
+    calendar.value = "";
+
+    const taskObj = {
+        value,
+        dateFormat,
+      };
+    
+    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+    taskList.push(taskObj);
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+
+    const task = createTask(taskObj);
+    list.appendChild(task);
+  };
+  
+    export const createTask = ({value,dateFormat}) => {
     const task = document.createElement('li');
     task.classList.add('card');
-    input.value = '';
-    //backticks
     const taskContent = document.createElement('div');
-  
-    const taskObj = {
-      value,
-      dateFormat,
-    };
-  
-    taskList.push(taskObj);
-  
-    localStorage.setItem('tasks', JSON.stringify(taskList));
+
   
     const titleTask = document.createElement('span');
     titleTask.classList.add('task');
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-import { addTask } from "./components/addTask.js"
-const btn=document.querySelector("[data-form-btn]")
+import { addTask } from "./components/addTask.js";
+import { readTasks} from "./components/readTasks.js";
+const btn=document.querySelector("[data-form-btn]");
 
 
 btn.addEventListener("click",addTask)
 
+readTasks();
